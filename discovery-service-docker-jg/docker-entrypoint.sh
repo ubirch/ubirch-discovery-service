7d496f9e411d6bb752f04e3be45684fea046f93c
sharkgua <benoit.george@ubirch.com>
2019-06-07 08:53:14 +0200

    Changed docker configuration for JG

--- discovery-service-docker-jg/docker-entrypoint.sh
+++ discovery-service-docker-jg/docker-entrypoint.sh
@@ -14,10 +14,11 @@ fi
 
 # running as non root user
 if [ "$1" == 'janusgraph' ]; then
+
   # setup config directory
-  # mkdir -p ${JANUS_DATA_DIR} ${JANUS_CONFIG_DIR}
+  mkdir -p ${JANUS_DATA_DIR} ${JANUS_CONFIG_DIR}
   # cp conf/gremlin-server/janusgraph-${JANUS_PROPS_TEMPLATE}-server.properties ${JANUS_CONFIG_DIR}/janusgraph.properties
-  # cp conf/gremlin-server/gremlin-server.yaml ${JANUS_CONFIG_DIR}
+  cp conf/gremlin-server/gremlin-server.yaml ${JANUS_CONFIG_DIR}
   chown -R "$(id -u):$(id -g)" ${JANUS_DATA_DIR} ${JANUS_CONFIG_DIR}
   chmod 700 ${JANUS_DATA_DIR} ${JANUS_CONFIG_DIR}
   chmod -R 600 ${JANUS_CONFIG_DIR}/*
@@ -43,12 +44,18 @@ if [ "$1" == 'janusgraph' ]; then
   done < <(env)
 
   # wait for storage
-  # if ! [ -z "${JANUS_STORAGE_TIMEOUT:-}" ]; then
-  #  F=$(mktemp --suffix .groovy)
-  #  echo "graph = JanusGraphFactory.open('${JANUS_CONFIG_DIR}/janusgraph.properties')" > $F
-  #  timeout ${JANUS_STORAGE_TIMEOUT}s bash -c \
-  #    "until bin/gremlin.sh -e $F > /dev/null 2>&1; do echo \"waiting for storage...\"; sleep 5; done"
-  # fi
+   if ! [ -z "${JANUS_STORAGE_TIMEOUT:-}" ]; then
+    F=$(mktemp --suffix .groovy)
+    echo "graph = JanusGraphFactory.open('${JANUS_CONFIG_DIR}/janusgraph-cql-server.properties')" > $F
+    timeout ${JANUS_STORAGE_TIMEOUT}s bash -c \
+      "until bin/gremlin.sh -e $F > /dev/null 2>&1; do echo \"waiting for storage...\"; sleep 5; done"
+   fi
+  cat ${JANUS_CONFIG_DIR}/gremlin-server.yaml
+  echo "------------------------------------------------------------"
+  cat ${JANUS_HOME}/bin/gremlin-server.sh
+  echo "------------------------------------------------------------"
+  echo "janusgraph.properties:"
+  cat ${JANUS_CONFIG_DIR}/janusgraph.properties
 
   exec ${JANUS_HOME}/bin/gremlin-server.sh ${JANUS_CONFIG_DIR}/gremlin-server.yaml
 fi
