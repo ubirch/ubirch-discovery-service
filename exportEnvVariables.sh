bda922b655a149a738ae0e6c53535590e84a1b8c
Carlos Sanchez <caaugustoss@gmail.com>
2019-12-09 12:39:41 +0100

    Removing useless script

--- exportEnvVariables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-#Script Helper to add Discovery Service Environment Keys to current session.
-#Usage:
-#source exportEnvVariables.sh
-
-export DISC_ENV_CORE_PORT=8182
-export DISC_ENV_CORE_ADDR=192.168.14.161
-
