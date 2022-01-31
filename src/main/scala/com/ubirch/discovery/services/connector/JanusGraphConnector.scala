8c1e5ab0626584c62b3c6a84000f0c3c028e8204
fuzyco <bkn4xuimp.gvr2kg.e@gmail.com>
2022-01-31 17:56:47 +0100

    delete unnecessary code

--- src/main/scala/com/ubirch/discovery/services/connector/JanusGraphConnector.scala
+++ src/main/scala/com/ubirch/discovery/services/connector/JanusGraphConnector.scala
@@ -32,7 +32,6 @@ import scala.util.{ Failure, Success, Try }
 class JanusGraphConnector @Inject() (lifecycle: Lifecycle, config: Config) extends GremlinConnector with LazyLogging {
 
   val cluster: Cluster = buildCluster(config)
-  //val cluster: Cluster = Cluster.open(buildProperties(config))
 
   implicit val graph: ScalaGraph = EmptyGraph.instance.asScala.configure(_.withRemote(DriverRemoteConnection.using(cluster)))
 
@@ -41,49 +40,6 @@ class JanusGraphConnector @Inject() (lifecycle: Lifecycle, config: Config) exten
 
   def closeConnection(): Unit = {
     cluster.close()
-  }
-
-  def buildProperties(config: Config): PropertiesConfiguration = {
-    val conf = new PropertiesConfiguration()
-
-    val hosts: List[String] = config.getString("core.connector.hosts")
-      .split(",")
-      .toList
-      .map(_.trim)
-      .filter(_.nonEmpty)
-
-    conf.addProperty("hosts", hosts.asJava)
-    conf.addProperty("port", config.getString("core.connector.port"))
-    conf.addProperty("serializer.className", config.getString("core.connector.serializer.className"))
-    // no idea why the following line needs to be duplicated. Doesn't work without
-    // cf https://stackoverflow.com/questions/45673861/how-can-i-remotely-connect-to-a-janusgraph-server first answer, second comment ¯\_ツ_/¯
-    conf.addProperty("serializer.config.ioRegistries", config.getAnyRef("core.connector.serializer.config.ioRegistries").asInstanceOf[java.util.ArrayList[String]])
-    conf.addProperty("serializer.config.ioRegistries", config.getStringList("core.connector.serializer.config.ioRegistries"))
-
-    Try(config.getInt("settings.connectionPool.maxContentLength")) match {
-      case Success(value) => conf.addProperty("settings.connectionPool.maxContentLength", value)
-      case Failure(_) => conf.addProperty("settings.connectionPool.maxContentLength", 4096000)
-    }
-
-    val maxWaitForConnection = config.getInt("core.connector.connectionPool.maxWaitForConnection")
-    if (maxWaitForConnection > 0) conf.addProperty("connectionPool.maxWaitForConnection", maxWaitForConnection)
-
-    val reconnectInterval = config.getInt("core.connector.connectionPool.reconnectInterval")
-    if (reconnectInterval > 0) conf.addProperty("connectionPool.reconnectInterval", reconnectInterval)
-
-    val connectionMinSize = config.getInt("core.connector.connectionPool.minSize")
-    if (connectionMinSize > 0) conf.addProperty("connectionPool.minSize", connectionMinSize)
-
-    val connectionMaxSize = config.getInt("core.connector.connectionPool.maxSize")
-    if (connectionMaxSize > 0) conf.addProperty("connectionPool.maxSize", connectionMaxSize)
-
-    val nioPoolSize = config.getInt("core.connector.nioPoolSize")
-    if (nioPoolSize > 0) conf.addProperty("nioPoolSize", nioPoolSize)
-
-    val workerPoolSize = config.getInt("core.connector.workerPoolSize")
-    if (workerPoolSize > 0) conf.addProperty("workerPoolSize", workerPoolSize)
-
-    conf
   }
 
   def buildCluster(config: Config): Cluster = {
@@ -132,69 +88,4 @@ class JanusGraphConnector @Inject() (lifecycle: Lifecycle, config: Config) exten
     Future.successful(closeConnection())
   }
 
-}
-
-object App {
-  def main(args: Array[String]) = {
-    val conf = new util.HashMap[String, AnyRef]()
-    conf.put("serializer.config.ioRegistries", "org.janusgraph.graphdb.tinkerpop.JanusGraphIoRegistry")
-    val serializer = new GraphBinaryMessageSerializerV1()
-    serializer.configure(conf, null)
-    //val cluster = Cluster.build.addContactPoint("localhost").reconnectInterval(500).port(8182).serializer(serializer).create()
-    val config = ConfigFactory.load()
-    val cluster: Cluster = Cluster.open(buildProperties(config))
-
-    val graph = EmptyGraph.instance.asScala.configure(_.withRemote(DriverRemoteConnection.using(cluster)))
-
-    val g: TraversalSource = graph.traversal
-    val b: Bindings = Bindings.instance
-
-    println(cluster.availableHosts())
-    val v = graph.traversal.V()
-    graph.V().iterate()
-    println(v)
-  }
-
-  def buildProperties(config: Config): PropertiesConfiguration = {
-    val conf = new PropertiesConfiguration()
-
-    val hosts: List[String] = config.getString("core.connector.hosts")
-      .split(",")
-      .toList
-      .map(_.trim)
-      .filter(_.nonEmpty)
-
-    conf.addProperty("hosts", hosts.asJava)
-    conf.addProperty("port", config.getString("core.connector.port"))
-    conf.addProperty("serializer.className", config.getString("core.connector.serializer.className"))
-    // no idea why the following line needs to be duplicated. Doesn't work without
-    // cf https://stackoverflow.com/questions/45673861/how-can-i-remotely-connect-to-a-janusgraph-server first answer, second comment ¯\_ツ_/¯
-    conf.addProperty("serializer.config.ioRegistries", config.getAnyRef("core.connector.serializer.config.ioRegistries").asInstanceOf[java.util.ArrayList[String]])
-    conf.addProperty("serializer.config.ioRegistries", config.getStringList("core.connector.serializer.config.ioRegistries"))
-
-    Try(config.getInt("settings.connectionPool.maxContentLength")) match {
-      case Success(value) => conf.addProperty("settings.connectionPool.maxContentLength", value)
-      case Failure(_) => conf.addProperty("settings.connectionPool.maxContentLength", 4096000)
-    }
-
-    val maxWaitForConnection = config.getInt("core.connector.connectionPool.maxWaitForConnection")
-    if (maxWaitForConnection > 0) conf.addProperty("connectionPool.maxWaitForConnection", maxWaitForConnection)
-
-    val reconnectInterval = config.getInt("core.connector.connectionPool.reconnectInterval")
-    if (reconnectInterval > 0) conf.addProperty("connectionPool.reconnectInterval", reconnectInterval)
-
-    val connectionMinSize = config.getInt("core.connector.connectionPool.minSize")
-    if (connectionMinSize > 0) conf.addProperty("connectionPool.minSize", connectionMinSize)
-
-    val connectionMaxSize = config.getInt("core.connector.connectionPool.maxSize")
-    if (connectionMaxSize > 0) conf.addProperty("connectionPool.maxSize", connectionMaxSize)
-
-    val nioPoolSize = config.getInt("core.connector.nioPoolSize")
-    if (nioPoolSize > 0) conf.addProperty("nioPoolSize", nioPoolSize)
-
-    val workerPoolSize = config.getInt("core.connector.workerPoolSize")
-    if (workerPoolSize > 0) conf.addProperty("workerPoolSize", workerPoolSize)
-
-    conf
-  }
 }
